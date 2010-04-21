54a59056da9b2de4e1a6349a6bb04fa72d834716
Kenneth Reitz <me@kennethreitz.com>
2010-04-20 21:27:30 -0400

    taking shape

--- fabfile.py
+++ fabfile.py
@@ -1,59 +1,27 @@
 from fabric.api import *
-# Server Stuff
-
-# set(fab_user='alex', 
-#      fab_hosts=['wincplace.com'], 
-#      root='/sites/wincplace.com/httpdocs/', 
-#      site='wincplace')
-# 
-# def deploy():
-#     local('git archive --format=tar HEAD | gzip > $(site).tar.gz')
-#     run('rm -rf $(root)$(site)') 
-#     put('$(site).tar.gz', '$(root)$(site).tar.gz')
-#     run('cd $(root)$(site) && tar zxf $(site).tar.gz')
-#     restart()
-# 
-# def restart():
-#     run('sh $(root)$(site)/restart.sh')
-
-
-# Local Stuff
-
-
-
-
-def dev():
-	"""
-	Run development server
-	"""	
-	local("python ./placehub/manage.py runserver")
 
 def test():
+    """ Run nose/doc tests?
     """
-    Run nose tests?
-    """
-    local("python ./run_tests.py")
+    local("python ./tests/run_tests.py")
 
 def init():
-    """
-    Initialize a virtualenv in which to run tests against this
+    """ Initialize a virtualenv in which to run tests against this
     """
     local("virtualenv .")
-    local("pip install -E . django django-piston django-mediasync fabric django-mongokit")
+    local("pip install -E . fabric lxml nose")
 
 def activate():
+	""" Activate local virtualenv
 	"""
-    Activate local virtualenv
-    """
 	local("source ./bin/activate")
 
-def cleanpip():
+def clean():
+    """ Remove the pycs
     """
-    Remove the pycs
-    """
-    local("rm -fr *.pyc")
+    local("find . -name "*.pyc" -exec rm '{}' ';'")
 
-def cleanpip():
+def scrub():
     """
     Remove the cruft created by virtualenv and pip
     """
